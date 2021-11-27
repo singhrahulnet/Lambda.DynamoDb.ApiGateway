f835bd1c481e3f78d96b415efeaeffdfb3dfe773
singhrahulnet <singh.rahul.net@gmail.com>
2021-11-27 15:30:21 +1100

    integration test refactoring

--- test/ILV.Api.Tests/Integration/FunctionTest.cs
+++ test/ILV.Api.Tests/Integration/FunctionTest.cs
@@ -22,7 +22,8 @@ namespace ILV.Api.Tests.Integration
         IAmazonDynamoDB _dDBClient { get; }
 
         private readonly IMiningService _miningService;
-        
+        private readonly Functions sut;
+
         public FunctionTest()
         {
             _tableName = "ilv-nft-" + DateTime.Now.Ticks;
@@ -30,72 +31,82 @@ namespace ILV.Api.Tests.Integration
             SetupTableAsync().Wait();
 
             _miningService = new MiningService(new PersistenceService(_dDBClient, _tableName));
+            sut = new Functions(_miningService);
         }
 
         [Fact]
         public async Task APIReturns0WhenRequestingWithin30SecondsOfAddingNFT()
         {
-            Functions functions = new Functions(_miningService);
             var request = new APIGatewayProxyRequest();
             var context = new TestLambdaContext();
 
-            APIGatewayProxyResponse response = await functions.AddNFTAsync(request, context);
+            var response = await sut.AddNFTAsync(request, context);
             Assert.Equal(201, response.StatusCode);
 
             var nftId = response.Body;
-
             request = new APIGatewayProxyRequest
             {
                 PathParameters = new Dictionary<string, string> { { Functions.ID_QUERY_STRING_NAME, nftId } }
             };
             context = new TestLambdaContext();
-            response = await functions.GetNFTAsync(request, context);
+
+            response = await sut.GetNFTAsync(request, context);
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("0", response.Body);
         }
 
         [Fact]
-        public async Task APIReturnsNon0WhenRequestingWithin30To90SecondsOfAddingNFT()
+        public async Task APIReturnsNon0AndDeletesTheResourceWhenRequestingWithin30To90SecondsOfAddingNFT()
         {
-            Functions functions = new Functions(_miningService);
             var request = new APIGatewayProxyRequest();
             var context = new TestLambdaContext();
 
-            APIGatewayProxyResponse response = await functions.AddNFTAsync(request, context);
+            var response = await sut.AddNFTAsync(request, context);
             Assert.Equal(201, response.StatusCode);
 
             var nftId = response.Body;
+            request = new APIGatewayProxyRequest
+            {
+                PathParameters = new Dictionary<string, string> { { Functions.ID_QUERY_STRING_NAME, nftId } }
+            };
+            context = new TestLambdaContext();
+
+            Thread.Sleep(TimeSpan.FromSeconds(31));
+            response = await sut.GetNFTAsync(request, context);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.NotEqual("0", response.Body);
 
             request = new APIGatewayProxyRequest
             {
                 PathParameters = new Dictionary<string, string> { { Functions.ID_QUERY_STRING_NAME, nftId } }
             };
             context = new TestLambdaContext();
-            Thread.Sleep(TimeSpan.FromSeconds(31));
-            response = await functions.GetNFTAsync(request, context);
-            Assert.Equal(200, response.StatusCode);
-            Assert.NotEqual("0", response.Body);
+
+            response = await sut.GetNFTAsync(request, context);
+
+            Assert.Equal(400, response.StatusCode);
         }
 
         [Fact]
         public async Task APIReturns0WhenRequestingAfters90SecondsOfAddingNFT()
         {
-            Functions functions = new Functions(_miningService);
             var request = new APIGatewayProxyRequest();
             var context = new TestLambdaContext();
 
-            APIGatewayProxyResponse response = await functions.AddNFTAsync(request, context);
+            var response = await sut.AddNFTAsync(request, context);
             Assert.Equal(201, response.StatusCode);
 
             var nftId = response.Body;
-
             request = new APIGatewayProxyRequest
             {
                 PathParameters = new Dictionary<string, string> { { Functions.ID_QUERY_STRING_NAME, nftId } }
             };
             context = new TestLambdaContext();
+
             Thread.Sleep(TimeSpan.FromSeconds(90));
-            response = await functions.GetNFTAsync(request, context);
+            response = await sut.GetNFTAsync(request, context);
+
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("0", response.Body);
         }
@@ -103,26 +114,25 @@ namespace ILV.Api.Tests.Integration
         [Fact]
         public async Task APIReturnsNotFoundWhenRequestingInvalidId()
         {
-            Functions functions = new Functions(_miningService);
-
             var request = new APIGatewayProxyRequest
             {
                 PathParameters = new Dictionary<string, string> { { Functions.ID_QUERY_STRING_NAME, "Invalid_ID" } }
             };
-
             var context = new TestLambdaContext();
-            var response = await functions.GetNFTAsync(request, context);
+
+            var response = await sut.GetNFTAsync(request, context);
+
             Assert.Equal(404, response.StatusCode);
         }
 
         [Fact]
         public async Task APIReturnsBadRequestWhenNotSupplyingId()
         {
-            Functions functions = new Functions(_miningService);
             var request = new APIGatewayProxyRequest();
             var context = new TestLambdaContext();
 
-            var response = await functions.GetNFTAsync(request, context);
+            var response = await sut.GetNFTAsync(request, context);
+
             Assert.Equal(400, response.StatusCode);
         }
 
